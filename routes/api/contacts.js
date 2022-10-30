7749b3c32f7f0a749f04bbe88ad93b28c0f7d748
PawelPalasinski <pawel.palasinski@gmail.com>
2022-10-30 02:09:49 +0100

    get, add, remove - done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,55 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("../../models/contacts.js");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'PP message' })
-})
+// GET all contacts (http://localhost:3000/api/contacts/)
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contactsList = await listContacts();
+  res.status(200).json({ body: contactsList });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// GET contacts by id (e.g. http://localhost:3000/api/contacts/1)
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const data = await getContactById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// ADD new contact (Postman: Body -> select: raw, JSON)
 
-module.exports = router
+router.post("/", async (req, res, next) => {
+  const contact = await addContact(req.body);
+  const contactsList = await listContacts();
+  contactsList.push(contact);
+
+  res.status(201).json({
+    data: contact,
+  });
+});
+
+// DELETE a contavts from the list (e.g. http://localhost:3000/api/contacts/5)
+
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const deleteContact = await removeContact(contactId);
+  res.status(204).json();
+});
+
+// PUT - Contact update
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
